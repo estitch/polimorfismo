5cdac8edb1ba52cd24daf903e907e35d7b981b47
stitch <stitch@DESKTOP-NLI8JML.mshome.net>
2022-11-14 13:50:27 -0500

    se hizo la prueba de creacion de interfaz de clase y tipo

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -3,8 +3,10 @@ public class Pruebas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-
+		//creamos el objeto paki con polimorfismo de tipo
+		Perro Paki = new Chiguagua();
+		//creamos el objeto puka con polimosfismo de clase
+		Chiguagua puka = new Chiguagua();
 	}
 
 }
