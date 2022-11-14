6ebea026f1801faaeb809c3fef39cdde8d25115c
stitch <stitch@DESKTOP-NLI8JML.mshome.net>
2022-11-14 15:03:03 -0500

    cree el metodo reconocer para que reconozca el tipo de clase y haga su metodo en comun

--- src/Pruebas.java
+++ src/Pruebas.java
@@ -1,21 +1,29 @@
 
 public class Pruebas {
+	public static void reconocer(Libro a) {
+		a.leer();
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//creamos el objeto paki con polimorfismo de tipo
 		Perro Paki = new Chiguagua();
+		
 		//creamos el objeto puka con polimosfismo de clase
 		Chiguagua puka = new Chiguagua();
+		reconocer(puka);
+
 		
 		Libro petLearn = new Chiguagua();
-		petLearn.leer();
+		//petLearn.leer();
 		//deberia tener el metodo cuidar ya que petLearn es un chiguagua
-		((Perro) petLearn).cuidar();
+		//((Perro) petLearn).cuidar();
 		petLearn = new Caballo();
-		petLearn.leer();
+		//petLearn.leer();
 		//no puede usar el metodo cuidar debido a que no tiene implementacion en caballo
-		((Perro) petLearn).cuidar();
+		//((Perro) petLearn).cuidar();
+		reconocer(petLearn);
+		
 		
 		//Paki = new Caballo();
 		
