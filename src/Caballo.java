19a2b2daffab27333092f9956d8119b33f943ad5
stitch <stitch@DESKTOP-NLI8JML.mshome.net>
2022-11-14 14:24:37 -0500

    implementacion de la interfaz Libro y agregacion de una clase caballo

---
+++ src/Caballo.java
@@ -0,0 +1,8 @@
+
+public class Caballo implements Libro {
+	@Override
+	public void leer() {
+		System.out.println("Aprender a cuidar la casa");
+	}
+
+}
--- src/Chiguagua.java
+++ src/Chiguagua.java
@@ -1,9 +1,14 @@
 
-public class Chiguagua extends Perro{
+public class Chiguagua extends Perro implements Libro{
 	
 	@Override
 	public void cuidar() {
-		
+		System.out.println("Cuidar la casa");
+	}
+	
+	@Override
+	public void leer() {
+		System.out.println("Aprender a cuidar");
 	}
 
 }
