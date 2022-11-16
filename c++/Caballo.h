4a9e44e6787795d2bf64218422d3b939e3fa221f
estitch <epis2014.1@gmail.com>
2022-11-15 21:28:10 -0500

    Creamos la clase Caballo con su implementacion, no se puede implementar la funcion cuidar() porque no esta definida en la interfaz Libro

---
+++ c++/Caballo.h
@@ -0,0 +1,12 @@
+#ifndef Caballo_h
+#define Caballo_h
+#include <iostream>
+#include "Libro.h"
+class Caballo:virtual public Libro{
+public:
+  void aprender()override{
+    std::cout<<"caballo aprende a cuidar";
+  }
+};
+
+#endif
