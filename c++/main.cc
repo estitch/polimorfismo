52227ab8a30e1cf6eb77a02533b18a41fd7c7fdd
estitch <epis2014.1@gmail.com>
2022-11-15 22:04:55 -0500

    creacion de la clase main y no se puede crear un objeto

---
+++ c++/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "Libro.h"
+#include "Perro.h"
+#include "Chiguagua.h"
+#include "Pastor.h"
+#include "Caballo.h"
+int main(){
+  Chiguagua petLearn = new Chiguagua;
+  //Libro petLearn = new Chiguagua();
+}
